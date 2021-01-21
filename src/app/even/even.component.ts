68ee0e96d11127113f5dc36535650dbf52bb4152
Zilola Matasimova <zilola.matasimova@gmail.com>
2021-01-21 13:08:09 -0500

    add-styling-for-odds-evens

--- src/app/even/even.component.ts
+++ src/app/even/even.component.ts
@@ -1,9 +1,17 @@
-import { Component, EventEmitter, OnInit, Output, Input } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnInit,
+  Output,
+  Input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-even',
   templateUrl: './even.component.html',
   styleUrls: ['./even.component.css'],
+  // encapsulation: ViewEncapsulation.None,
 })
 export class EvenComponent implements OnInit {
   @Input() numberEven: number;
