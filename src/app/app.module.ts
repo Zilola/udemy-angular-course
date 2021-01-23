ec06e7e21903f2f7e64328582da9b1a4f7738ee2
Zilola Matasimova <zilola.matasimova@gmail.com>
2021-01-23 00:23:15 -0500

    using the renderer to build better attribute directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BasicHighlightDirective } from "./basic-highlight/basic-highlight.directive";
 import { BrowserModule } from "@angular/platform-browser";
-import { Directive, NgModule } from "@angular/core";
+import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
+import { BetterHighlightDirective } from "./better-highlight/better-highlight.directive";
 
 @NgModule({
-  declarations: [AppComponent, BasicHighlightDirective],
+  declarations: [
+    AppComponent,
+    BasicHighlightDirective,
+    BetterHighlightDirective,
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
