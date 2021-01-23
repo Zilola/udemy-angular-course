02423bcd3ce92abb74731f94c6becdd94ba34764
Zilola Matasimova <zilola.matasimova@gmail.com>
2021-01-23 00:48:05 -0500

    using hostlistener to listen host events

--- src/app/basic-highlight/basic-highlight.directive.ts
+++ src/app/basic-highlight/basic-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit } from "@angular/core";
+import { Directive, ElementRef, HostListener, OnInit } from "@angular/core";
 
 @Directive({
   selector: "[appBasicHighlight]",
@@ -7,6 +7,13 @@ export class BasicHighlightDirective implements OnInit {
   constructor(private elementRef: ElementRef) {}
 
   ngOnInit() {
+    // this.elementRef.nativeElement.style.backgroundColor = "green";
+  }
+  @HostListener("mouseenter") mouseover(eventData: Event) {
     this.elementRef.nativeElement.style.backgroundColor = "green";
+  }
+
+  @HostListener("mouseleave") mouseleave(eventData: Event) {
+    this.elementRef.nativeElement.style.backgroundColor = "transparent";
   }
 }
